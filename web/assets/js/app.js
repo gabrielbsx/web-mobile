8c6aa940e14bf3ab43f2d611773dcd35b2bcdd89
Gabriel <gabrielturing@gmail.com>
2022-10-17 23:52:36 -0300

    feat: vaccine grid

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,30 +1,36 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAO94rUeqLlJFAqGLjHKL1YH66nKZIfEv8',
-  authDomain: 'health-b778f.firebaseapp.com',
-  projectId: 'health-b778f',
-  storageBucket: 'health-b778f.appspot.com',
-  messagingSenderId: '580812422008',
-  appId: '1:580812422008:web:2d259fb2bd5b9564bee68d',
-  measurementId: 'G-MP2C7X9YC4'
-};
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
-const app = firebase.initializeApp(firebaseConfig);
-const auth = app.auth();
-const db = app.firestore();
+import { app, auth } from "../../assets/js/config.js";
 
 const nameValidation = (name) => name.length > 1;
-const genderValidation = (gender) => gender === 'masculino' || gender === 'feminino';
-const passwordConfirmation = (password, passwordConfirm) => password === passwordConfirm;
+const genderValidation = (gender) =>
+  gender === "masculino" || gender === "feminino";
+const passwordConfirmation = (password, passwordConfirm) =>
+  password === passwordConfirm;
 const bornValidation = (born) => born < new Date();
-const emailValidation = (email) => email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+const emailValidation = (email) =>
+  email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
 
 const setMessageError = (message) => {
-  const errorDiv = document.querySelector('div.input-error');
-  const errorMessage = document.createElement('small');
+  const errorDiv = document.querySelector("div.input-error");
+  const errorMessage = document.createElement("small");
   errorMessage.textContent = message;
-  errorMessage.classList.add('text-error');
+  errorMessage.classList.add("text-error");
   errorDiv.appendChild(errorMessage);
-}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  onAuthStateChanged(auth, (user) => {
+    const paths = ['index.html'];
+    switch (window.location.pathname) {
+      case '/index.html':
+        window.location.pathname = "/vaccine.html";    
+    }
+  });
+});
 
 const signInController = (event) => {
   // previnir a ação default do formulário, que é o envio dos dados no action com o method e um refresh na página
@@ -33,17 +39,16 @@ const signInController = (event) => {
   // puxa os dados dos respectivos inputs
   const emailInput = document.querySelector('input[name="email"]');
   const passwordInput = document.querySelector('input[name="password"]');
-  
+
   // destructuring data
   const { value: email } = emailInput;
   const { value: password } = passwordInput;
-  
+
   // criando um objeto usuário com os respectivos dados
   const user = { email, password };
 
-
-
-}
+  console.log(user);
+};
 
 const signUpHandle = (event) => {
   event.preventDefault();
@@ -53,9 +58,11 @@ const signUpHandle = (event) => {
   const born = document.querySelector('input[name="nascimento"]').value;
   const email = document.querySelector('input[name="email"]').value;
   const password = document.querySelector('input[name="password"]').value;
-  const passwordConfirm = document.querySelector('input[name="passwordConfirm"]').value;
-  const errorDiv = document.querySelector('div.input-error');
-  errorDiv.innerHTML = '';
+  const passwordConfirm = document.querySelector(
+    'input[name="passwordConfirm"]'
+  ).value;
+  const errorDiv = document.querySelector("div.input-error");
+  errorDiv.innerHTML = "";
 
   const user = {
     name,
@@ -65,32 +72,33 @@ const signUpHandle = (event) => {
   };
 
   if (!nameValidation(name)) {
-    setMessageError('Nome inválido');
+    setMessageError("Nome inválido");
     return;
   }
   if (!genderValidation(user.gender)) {
-    setMessageError('Sexo inválido');
+    setMessageError("Sexo inválido");
     return;
   }
   if (!emailValidation(email)) {
-    setMessageError('E-mail inválido');
+    setMessageError("E-mail inválido");
     return;
   }
   if (password.length < 6) {
-    setMessageError('Senha deve conter 6 caracteres');
+    setMessageError("Senha deve conter 6 caracteres");
     return;
   }
   if (!passwordConfirmation(password, passwordConfirm)) {
-    setMessageError('Senha não confere');
+    setMessageError("Senha não confere");
     return;
   }
   if (!bornValidation(user.born)) {
-    setMessageError('Data de nascimento inválida');
+    setMessageError("Data de nascimento inválida");
     return;
   }
 
-  auth.createUserWithEmailAndPassword(email, password)
-    .then(credential => {
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((credential) => {
       console.log(credential, credential.user.uid);
       credential.user.updateProfile({
         displayName: name,
@@ -100,7 +108,7 @@ const signUpHandle = (event) => {
       // auth.currentUser.updateProfile({
       //   displayName: name,
       // });
-      db.collection('users')
+      db.collection("users")
         .doc(credential.user.uid)
         .set({
           displayName: name,
@@ -110,47 +118,52 @@ const signUpHandle = (event) => {
         .then((data) => {
           console.log(data);
         })
-        .catch(error => {
-          console.log('error');
+        .catch((error) => {
+          console.log("error");
         });
-      alert('Conta criada com sucesso!');
+      alert("Conta criada com sucesso!");
     })
     .then(() => {
       // window.location.pathname = '/index.html';
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
-      setMessageError('Houve algum erro ao efetuar o cadastro');
+      setMessageError("Houve algum erro ao efetuar o cadastro");
     });
-}
+};
 
 const recoveryHandle = (event) => {
   event.preventDefault();
-  
+
   const email = document.querySelector('input[name="email"]').value;
-  const errorDiv = document.querySelector('div.input-error');
-  errorDiv.innerHTML = '';
+  const errorDiv = document.querySelector("div.input-error");
+  errorDiv.innerHTML = "";
 
-  auth.sendPasswordResetEmail(email)
+  auth
+    .sendPasswordResetEmail(email)
     .then(() => {
-      alert('Enviamos sua nova senha em seu e-mail!');
+      alert("Enviamos sua nova senha em seu e-mail!");
     })
-    .catch(error => {
+    .catch((error) => {
       setMessageError(error.message);
-    })
-}
+    });
+};
 
-const signInHandle = (event) => {
+export const signInHandle = (event) => {
   event.preventDefault();
 
   const email = document.querySelector('input[name="email"]').value;
   const password = document.querySelector('input[name="password"]').value;
 
-  auth.signInWithEmailAndPassword(email, password)
-    .then(credential => {
-      console.log(credential);
+  signInWithEmailAndPassword(auth, email, password)
+    .then((credential) => {
+      window.location.pathname = "/vaccine.html";
     })
-    .catch(error => {
-      setMessageError(error.message);
-    })
-}
+    .catch((error) => {
+      setMessageError("E-mail e/ou senha inválidos.");
+    });
+};
+
+document.querySelector(".form-button").addEventListener("click", (event) => {
+  signInHandle(event);
+});
---
+++ web/assets/js/config.js
@@ -0,0 +1,15 @@
+import { initializeApp }  from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth }  from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO94rUeqLlJFAqGLjHKL1YH66nKZIfEv8",
+  authDomain: "health-b778f.firebaseapp.com",
+  projectId: "health-b778f",
+  storageBucket: "health-b778f.appspot.com",
+  messagingSenderId: "580812422008",
+  appId: "1:580812422008:web:2d259fb2bd5b9564bee68d",
+  measurementId: "G-MP2C7X9YC4",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
